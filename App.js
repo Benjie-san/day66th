04f53f98f5a8ddfaa6b91f10bba8fc959e931f92
Benjie-san <haisesasaki56@gmail.com>
2024-07-01 15:18:06 +0800

    test 2

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
       <Text>DAY 66th</Text>
       <Text>Progress Tracker</Text>
+
+      </View>
+      <View>
+        <Text>ITEM</Text>
+      </View>
 
 
       <StatusBar style="auto" />
